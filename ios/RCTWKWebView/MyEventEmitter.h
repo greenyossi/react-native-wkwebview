9d8de57c165914ffbbfa29436d126b737409e50e
Jacob Barr <kobi.bar@onoapps.com>
2018-08-02 14:44:12 +0300

    bridge implementation.

---
+++ ios/RCTWKWebView/MyEventEmitter.h
@@ -0,0 +1,24 @@
+//
+//  MyEventEmitter.h
+//  RCTWKWebView
+//
+//  Created by Jacob Barr on 30/07/2018.
+//
+
+#ifndef MyEventEmitter_h
+#define MyEventEmitter_h
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+@interface MyEventEmitter : RCTEventEmitter <RCTBridgeModule>
+
+- (void) sendEventWithFileName: (NSString *)fileName;
+
+- (void) sendEventWithFileName: (NSString *)fileName withMimeType: (NSString *)mimeType
+
+@end
+
+
+#endif /* MyEventEmitter_h */
+
---
+++ ios/RCTWKWebView/MyEventEmitter.m
@@ -0,0 +1,42 @@
+//
+//  MyEventEmitter.m
+//  RCTWKWebView
+//
+//  Created by Jacob Barr on 30/07/2018.
+//
+
+#import <Foundation/Foundation.h>
+#import "MyEventEmitter.h"
+
+@implementation MyEventEmitter
+
+RCT_EXPORT_MODULE();
+
++ (id)allocWithZone:(NSZone *)zone {
+    static MyEventEmitter *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [super allocWithZone:zone];
+    });
+    return sharedInstance;
+}
+
+- (void) sendEventWithFileName: (NSString *)fileName
+{
+    [self sendEventWithName:@"iosFileEvent" body:@{@"fileName": fileName}];
+}
+
+- (void) sendEventWithFileName: (NSString *)fileName withMimeType: (NSString *)mimeType
+{
+    [self sendEventWithName:@"iosFileEvent" body:@{@"fileName": fileName, @"mimeType": mimeType}];
+}
+
+
+- (NSArray<NSString *> *)supportedEvents
+{
+    return @[@"iosFileEvent"];
+}
+
+
+@end
+
